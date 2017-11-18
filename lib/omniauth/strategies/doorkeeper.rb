e5d9b2ca82c7664fc5c019292a12062b9f0de82b
a.harada <redhornet96@gmail.com>
2017-11-18 14:52:55 +0900

    doorkeeper client sample

---
+++ lib/omniauth/strategies/doorkeeper.rb
@@ -0,0 +1,28 @@
+module OmniAuth
+  module Strategies
+    class Doorkeeper < OmniAuth::Strategies::OAuth2
+      option :name, :doorkeeper
+      option :client_options, site: 'http://localhost:3000', authorize_path: '/oauth/authorize'
+
+      uid { raw_info['id'] }
+      # providerから送られてきたデータの内、どれを使いたいか
+      info do
+        { email: raw_info['email'] }
+      end
+
+      # providerのAPIを叩いて、データを取ってくる
+      # def raw_info
+      #   @raw_info ||= access_token.get('/api/login.json').parsed
+      # end
+
+      def raw_info
+        binding.pry
+        @raw_info ||= JSON.parse(access_token.get('api/v1/me').response.body)
+      end
+
+      def callback_url
+        full_host + script_name + callback_path
+      end
+    end
+  end
+end
